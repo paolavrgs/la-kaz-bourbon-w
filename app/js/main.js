bad37cf7b573c72921422b63732e737e4f776b37
Paola Vargas <vrgspaola68@gmail.com>
2021-02-27 02:19:48 -0400

    Add missing elements. Fix styles. Responsive. Add 404 and contact us page with responsive.

--- app/js/main.js
+++ app/js/main.js
@@ -19,11 +19,6 @@ $(".product-images-slider").slick({
     "<button class='slick-next slick-arrow' aria-label='Next' type='button'><img class='slick-next' src='assets/img/icons/arrow_right.svg'></button>"
 });
 
-$('.modal').on('shown.bs.modal', function (e) {
-  $('.product-images-slider').slick('setPosition');
-  $('.wrap-modal-slider').addClass('open');
-})
-
 $(".gallery-images-slider").slick({
   infinite: true,
   slidesToShow: 1,
@@ -33,7 +28,7 @@ $(".gallery-images-slider").slick({
   prevArrow:
     "<button class='slick-prev slick-arrow' aria-label='Previous' type='button'><img class='slick-prev' src='assets/img/icons/arrow_left.svg'></button>",
   nextArrow:
-    "<button class='slick-next slick-arrow' aria-label='Next' type='button'><img class='slick-next' src='assets/img/icons/arrow_right.svg'></button>",
+    "<button class='slick-next slick-arrow' aria-label='Next' type='button'><img class='slick-next' src='assets/img/icons/arrow_right.svg'></button>"
 });
 
 new WOW().init();
@@ -56,18 +51,27 @@ $(window).on("scroll", () => {
     $(".js-gotop").fadeOut();
   }
 
-  const $el = $('.widget-scroll'); 
-  const isPositionFixed = ($el.css('position') == 'fixed');
-  if ($(this).scrollTop() > 600 && !isPositionFixed){ 
-    $el.css({'position': 'fixed', 'top': '0px', 'display': 'block'}); 
+  const $el = $(".widget-scroll");
+  const isPositionFixed = $el.css("position") == "fixed";
+  if ($(this).scrollTop() > 600 && !isPositionFixed) {
+    $el.css({ position: "fixed", top: "0px", display: "block" });
   }
-  if ($(this).scrollTop() < 600 && isPositionFixed){
-    $el.css({'position': 'static', 'top': '0px', 'display': 'none'}); 
+  if ($(this).scrollTop() < 600 && isPositionFixed) {
+    $el.css({ position: "static", top: "0px", display: "none" });
   }
 });
 
 $(".js-gotop").on("click", () => {
   $("html, body").animate({ scrollTop: 0 }, 500);
+});
+
+$(".banner-arrow-down").on("click", () => {
+  $("html, body").animate(
+    {
+      scrollTop: $(".home-map").offset().top - 100
+    },
+    500
+  );
 });
 
 // BANNER VIDEO CONTROLS
